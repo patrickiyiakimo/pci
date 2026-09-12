ae9eb7c9dcb95215d655e60e4523019202e459e8
patrickiyiakimo <iyiakimopatrick2002@gmail.com>
2026-09-12 23:04:51 +0100

    feat: Added the smartsupp chat box

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import HeaderContainer from "@/components/containers/HeaderContainer";
 import FooterContainer from "@/components/containers/FooterContainer";
 
@@ -10,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className="h-full antialiased" data-scroll-behavior="smooth">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -28,6 +29,25 @@ export default function RootLayout({ children }) {
         <HeaderContainer />
         <main className="flex-1">{children}</main>
         <FooterContainer />
+        <Script
+          id="smartsupp-live-chat"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _smartsupp = _smartsupp || {};
+              _smartsupp.key = '78fd41b6d164c2b656dbeee4f15167d7a962995d';
+              window.smartsupp||(function(d) {
+                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                c.type='text/javascript';c.charset='utf-8';c.async=true;
+                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+              })(document);
+            `,
+          }}
+        />
+        <noscript>
+          Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a>
+        </noscript>
       </body>
     </html>
   );
